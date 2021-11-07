dc7bad129a13525a49534bfe907c1cb3ac4f8946
EC2 Default User <ec2-user@ip-172-31-27-1.ec2.internal>
2021-11-07 11:25:03 +0000

    [Fix]3-9 bookersアプリのRSpecエラーの解消

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,9 +30,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    flash[:notice] = "Book was successfully update."
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully update."
+      redirect_to book_path(@book.id)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
